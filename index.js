cde463a6c49f82092087b62abcca07cc5c0691f8
nayzawaungnza <nayzawaung750@gmail.com>
2024-05-31 09:43:46 +0630

    Ep-3-Global-Object

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello node js");
+//console.log(global);
+
+let interval = global.setInterval(() => {
+  console.log("hello");
+}, 1000);
+global.setTimeout(() => {
+  clearInterval(interval);
+}, 3000);
+
+console.log(__dirname);
+console.log(__filename);
