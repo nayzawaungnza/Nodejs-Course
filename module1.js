c8e6ba5e8586a192ada2cedfa5182ad67c88f912
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-03 09:02:20 +0630

    Ep-4-Node-Module-System

---
+++ module1.js
@@ -0,0 +1,5 @@
+const myname = "nay zaw aung";
+const myage = 29;
+const another = "one";
+
+module.exports = { myname: myname, myage: myage, another: another };
---
+++ module2.js
@@ -0,0 +1,3 @@
+const { myname, myage, another } = require("./module1");
+
+console.log(myname, myage, another);
