c85410d16fb4e33f0c6fb19de9a40af762a3814e
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-04 11:12:52 +0630

    Ep-14-Expess-routing-basics

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { dirname } = require("path");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  //response.send("<h1>Hello Home</h1>");
+  response.sendFile("./views/home.html", { root: __dirname });
+});
+
+app.get("/about", (request, response) => {
+  //response.send("<h1>Hello about</h1>");
+  response.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/contact", (request, response) => {
+  //response.send("<h1>Hello about</h1>");
+  response.sendFile("./views/contact.html", { root: __dirname });
+});
+
+//app.listen(3000);
+app.listen(3000, () => {
+  console.log("App is running in port 3000.");
+});
