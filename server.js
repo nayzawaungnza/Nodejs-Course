a19594575080382e19de98a4da0db5bec1b29a76
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-04 10:23:50 +0630

    Ep-10-Http-Status-Codes

--- server.js
+++ server.js
@@ -7,16 +7,20 @@ const server = http.createServer((request, response) => {
   switch (request.url) {
     case "/":
       filename = "home.html";
+      response.statusCode = 200;
       break;
     case "/about":
       filename = "about.html";
+      response.statusCode = 200;
       break;
     case "/contact":
       filename = "contact.html";
+      response.statusCode = 200;
       break;
 
     default:
       filename = "404.html";
+      response.statusCode = 404;
       break;
   }
 
