798fb200c8aedef1df22aef94d63b6ff1c9285f3
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-04 10:16:36 +0630

    Ep-9-Basic-Routing

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ const fs = require("fs");
 
 const server = http.createServer((request, response) => {
   console.log("request make from client");
+  let filename;
+  switch (request.url) {
+    case "/":
+      filename = "home.html";
+      break;
+    case "/about":
+      filename = "about.html";
+      break;
+    case "/contact":
+      filename = "contact.html";
+      break;
+
+    default:
+      filename = "404.html";
+      break;
+  }
 
   response.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/home.html", (error, data) => {
+  fs.readFile("./views/" + filename, (error, data) => {
     if (error) {
       console.log(error);
     } else {
