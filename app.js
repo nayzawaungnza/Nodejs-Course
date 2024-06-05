e0590f751fa90b5c98ff6e604512f4dd03955dee
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-05 08:40:26 +0630

    Ep-15-Express-redirection-and-404

--- app.js
+++ app.js
@@ -13,9 +13,16 @@ app.get("/about", (request, response) => {
   response.sendFile("./views/about.html", { root: __dirname });
 });
 
+app.get("/about-us", (request, response) => {
+  response.redirect("/about");
+});
+
 app.get("/contact", (request, response) => {
   //response.send("<h1>Hello about</h1>");
   response.sendFile("./views/contact.html", { root: __dirname });
+});
+app.use((request, response) => {
+  response.sendFile("./views/404.html", { root: __dirname });
 });
 
 //app.listen(3000);
