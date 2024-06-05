b5f9c772e593377c55a75e0fe505cb328eea0e76
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-05 09:26:25 +0630

    Ep-19-Dynamic-title

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ let blogs = [
 app.get("/", (request, response) => {
   //response.send("<h1>Hello Home</h1>");
   //response.sendFile("./views/home.html", { root: __dirname });
-  response.render("home", { blogs: blogs });
+  response.render("home", { blogs: blogs, title: "Home" });
 });
 
 app.get("/about", (request, response) => {
   //response.send("<h1>Hello about</h1>");
   //response.sendFile("./views/about.html", { root: __dirname });
-  response.render("about");
+  response.render("about", { title: "About" });
 });
 
 app.get("/about-us", (request, response) => {
@@ -30,11 +30,11 @@ app.get("/about-us", (request, response) => {
 app.get("/contact", (request, response) => {
   //response.send("<h1>Hello about</h1>");
   //response.sendFile("./views/contact.html", { root: __dirname });
-  response.render("contact");
+  response.render("contact", { title: "Contact" });
 });
 app.use((request, response) => {
   //response.sendFile("./views/404.html", { root: __dirname });
-  response.status(404).render("404");
+  response.status(404).render("404", { title: "404" });
 });
 
 //app.listen(3000);
