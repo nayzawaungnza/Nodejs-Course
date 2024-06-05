3fd5931f92a4d108077d1e5ad96910b74c17245a
nayzawaungnza <nayzawaung750@gmail.com>
2024-06-05 08:52:52 +0630

    Ep-16-Ejs-View-Engine

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require("express");
 const { dirname } = require("path");
 
 const app = express();
+// using ejs package
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
   //response.send("<h1>Hello Home</h1>");
-  response.sendFile("./views/home.html", { root: __dirname });
+  //response.sendFile("./views/home.html", { root: __dirname });
+  response.render("home");
 });
 
 app.get("/about", (request, response) => {
   //response.send("<h1>Hello about</h1>");
-  response.sendFile("./views/about.html", { root: __dirname });
+  //response.sendFile("./views/about.html", { root: __dirname });
+  response.render("about");
 });
 
 app.get("/about-us", (request, response) => {
@@ -19,10 +24,12 @@ app.get("/about-us", (request, response) => {
 
 app.get("/contact", (request, response) => {
   //response.send("<h1>Hello about</h1>");
-  response.sendFile("./views/contact.html", { root: __dirname });
+  //response.sendFile("./views/contact.html", { root: __dirname });
+  response.render("contact");
 });
 app.use((request, response) => {
-  response.sendFile("./views/404.html", { root: __dirname });
+  //response.sendFile("./views/404.html", { root: __dirname });
+  response.status(404).render("404");
 });
 
 //app.listen(3000);
